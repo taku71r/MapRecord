a621d8e6d8e3a55da0733241565169062c497b7e
Takuroh Shimizu <his7716twd@gmail.com>
2020-05-23 17:55:23 +0900

    Initial Commit

---
+++ MapRecord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapRecord
+//
+//  Created by 清水拓郎 on 2020/05/23.
+//  Copyright © 2020 清水拓郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MapRecordTests/MapRecordTests.swift
@@ -0,0 +1,34 @@
+//
+//  MapRecordTests.swift
+//  MapRecordTests
+//
+//  Created by 清水拓郎 on 2020/05/23.
+//  Copyright © 2020 清水拓郎. All rights reserved.
+//
+
+import XCTest
+@testable import MapRecord
+
+class MapRecordTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
