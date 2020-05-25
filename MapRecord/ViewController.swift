89576d428d2b53ba8d7af57eb2c705d15e8e6aea
Takuroh Shimizu <his7716twd@gmail.com>
2020-05-25 22:32:29 +0900

    地図機能の実験

--- MapRecord/ViewController.swift
+++ MapRecord/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+//MapKit frameworkをインポートする
+import MapKit
+//現在地取得用
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    
+    @IBOutlet var mapView: MKMapView!
+    var locManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locManager = CLLocationManager()
+        locManager.delegate = self
+        
+        //位置情報の使用の許可を得る
+        locManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled(){
+            switch CLLocationManager.authorizationStatus() {
+            case .authorizedWhenInUse:
+                //座標の表示
+                locManager.startUpdatingLocation()
+                break
+            default:
+                break
+           
+            }
+        }
+        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+            let lonStr = (locations.last?.coordinate.longitude.description)!
+            let latStr = (locations.last?.coordinate.latitude.description)!
+            
+            print("lon :" + lonStr)
+            print("lat :" + latStr)
+        }
+        
+        
     }
 
 
